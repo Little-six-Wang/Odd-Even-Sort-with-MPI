9fc9ff157d9a2925dc749936cbef4919634617b2
Taoer <82244211+Little-six-Wang@users.noreply.github.com>
2023-11-27 10:22:10 +0800

    Update test.c

--- serial/test.c
+++ serial/test.c
@@ -1,6 +1,35 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include"parameters.h"
+
+double dclock();
+void createVec(int, long*);
+void sort(int, long*);
 
 int main(){
-  printf("hello!\n");
+  double time_used;
+  long* vec = NULL;
+  
+  vec = malloc(N * sizeof(long));
+  
+  createVec(N, vec);
+  
+  printf("serial = [\n");
+  for(int i = 0; i < N; i++){
+    printf("%ld ", vec[i]);
+  }
+  
+  time_used = dclock();
+  sort(N, vec);
+  time_used = dclock() - time_used;
+  
+  printf(";\n");
+  for(int i = 0; i < N; i++){
+    printf("%ld ", vec[i]);
+  }
+  printf("]\n");
+  
+  printf("serial_sec = %lf\n", time_used * 1000);
+  
   return 0;
 }
